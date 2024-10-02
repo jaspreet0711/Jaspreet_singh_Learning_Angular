05344ffe7166bd35e217fbf111b7780c6f5a78c1
Jaspreet Singh <w0847545@myscc.ca>
2024-10-01 20:02:16 -0400

    Step 8 & 9 completed here.

--- src/app/cricket-list/cricket-list.component.ts
+++ src/app/cricket-list/cricket-list.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NgForOf} from "@angular/common";
 import {CricketListItemComponent} from "../cricket-list-item/cricket-list-item.component";
 import {CricketPlayerService} from "../services/cricket-player.service";
+import {Cricket} from "../Shared/Models/Cricket";
 
 @Component({
   selector: 'app-cricket-list',
@@ -10,8 +11,23 @@ import {CricketPlayerService} from "../services/cricket-player.service";
   templateUrl: './cricket-list.component.html',
   styleUrl: './cricket-list.component.scss'
 })
-export class CricketListComponent {
+export class CricketListComponent implements OnInit{
+
+  // placeholder values
+  columns:string[]= ['playerName', 'age', 'team', 'jerseyNumber', 'isRetired'];
+  Players: Cricket[] = [];
+
 
   constructor(private playerService: CricketPlayerService) {
   } // Dependency injection here
+
+
+  ngOnInit() {
+    this.playerService.getPlayerList().subscribe({
+      next: (data: Cricket[]) => this.Players = data,
+      error:err => console.error("Error fetching player list", err),
+      complete:() => console.log("Player List fetched successfully!")
+    })
+  }
+
 }
--- src/app/services/cricket-player.service.ts
+++ src/app/services/cricket-player.service.ts
@@ -15,4 +15,37 @@ export class CricketPlayerService {
     return of(Players);
   }
 
+  // creating my CRUD methods here
+  /**
+   * I have player name as main value in my code
+   * So I'm going to replace the condition to Add/update/delete/read methods by using player name not by id or number
+   * @param newPlayer
+   */
+  // add player method
+  addPlayer(newPlayer:Cricket) : Observable<Cricket[]>{
+    this.playerList.push(newPlayer)
+    return of(this.playerList);
+  }
+
+  //update player method
+  updatePlayer(updatedPlayer: Cricket): Observable<Cricket[]>{
+    const index = this.playerList.findIndex(player => player.playerName === updatedPlayer.playerName);
+    if (index !== -1) {
+      this.playerList[index] = updatedPlayer;
+    }
+    return of(this.playerList);
+  }
+
+  // delete method here
+  deletePlayer(name: string): Observable<Cricket[]>{
+    this.playerList = this.playerList.filter(player => player.playerName !== name);
+    return of(this.playerList);
+  }
+
+  // read players method
+  getPlayerByName(name: string): Observable<Cricket | undefined>{
+    const member = this.playerList.find(player => player.playerName === name);
+    return of(member);
+  }
+
 }
