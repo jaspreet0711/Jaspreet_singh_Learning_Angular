c78c1ce551baf2df73ff16910841acee92845952
Jaspreet Singh <w0847545@myscc.ca>
2024-10-09 13:10:23 -0400

    I realised that I forget to change my branch to assignment 5 before working on routes. So, now I have fixed my all issues on this branch and going to start working on routes in assignment 5 branch.

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import {provideRouter, Routes} from "@angular/router";
-import {CricketListComponent} from "./app/cricket-list/cricket-list.component";
-import {CricketListItemComponent} from "./app/cricket-list-item/cricket-list-item.component";
+import {appConfig} from "./app/app.config";
 
 
-// defining routes of my files here
-const routes: Routes = [
-  {path: 'players', component:CricketListComponent},
-  {path: 'players/:playerName', component: CricketListItemComponent}
-];
 
-bootstrapApplication(AppComponent,{
-  providers: [provideRouter(routes)]
-})
+
+bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
